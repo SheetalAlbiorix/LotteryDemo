cadf036fde190c9b5b52a3aa7d361f15cc61b6a2
ankur <ankur.akvaliya1793@gmail.com>
2021-07-26 15:39:54 +0530

    - add strings

--- lib/constants/Constant.dart
+++ lib/constants/Constant.dart
@@ -10,3 +10,15 @@ const Color grayBgColor = Color(0xFF423b41);
 const Color blackTextColor = Color(0xFF14161c);
 const Color whiteColor = Color(0xFFffffff);
 const Color lightWhiteTextColor = Color(0xFF998098);
+
+
+const weeklyLotteryText="Weekly Lottery";
+const playNowChanceText="Play now for your chance to win big every week! Submit entries before time expires";
+const jackpotText="JACKPOT";
+const remainingText="Remaining";
+const myEntriesText="MY ENTRIES";
+const submitEntryText="Submit entry";
+const thisWeekWinningNumberText="THIS WEEK'S WINNING NUMBERS";
+const youWonText="YOU WON!";
+const whatsNextText="What's next!";
+const youEmployerText="You Employer has been notified of your win and will be delivering your prize soon. If you have any questions please contact your supervisions. Thanks for playing!";
