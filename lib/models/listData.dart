4accc733cddf47bb5177ad356f777434ee05ec08
Jalpesh009 <jalpesh@albiorixtech.com>
2021-07-27 12:29:55 +0530

    working on dashboard page

--- lib/models/listData.dart
+++ lib/models/listData.dart
@@ -2,18 +2,10 @@ class ListData {
   ListData({
     this.title,
     this.isSelected,
+    this.isAnimated = false,
   });
 
   String? title;
   bool? isSelected;
-
-  factory ListData.fromMap(Map<String, dynamic> json) => ListData(
-    title: json["id"] == null ? null : json["id"],
-    isSelected: json["name"] == null ? null : json["name"],
-  );
-
-  Map<String, dynamic> toMap() => {
-    "id": title == null ? null : title,
-    "name": isSelected == null ? null : isSelected,
-  };
+  bool? isAnimated;
 }
